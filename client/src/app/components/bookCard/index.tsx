25215281f4d9d4686a6546b538e99e76f3d18049
hyunjin <leehj0110@kakao.com>
2021-06-13 00:02:22 +0900

    add marginer bookCard

---
+++ client/src/app/components/bookCard/index.tsx
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+import { Button } from "../button";
+import {faCalendarAlt} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { Marginer } from "../marginer";
+
+
+const CardContainer = styled.div`
+    ${tw`
+        flex
+        justify-center
+        items-center
+        rounded-md
+        bg-white
+        pt-1
+        pb-1
+        pr-2
+        pl-2
+        md:pt-2
+        md:pb-2
+        md:pl-6
+        md:pr-6
+
+    `}
+`
+
+const ItemContainer = styled.div`
+    ${tw`
+        flex
+    `}
+`
+
+const Icon = styled.span`
+    ${tw`
+        text-red-500
+        fill-current
+        text-xs
+        md:text-base
+        mr-1
+        md:mr-3
+
+    `}
+`
+
+const Name = styled.span`
+    ${tw`
+        text-gray-600
+        text-xs
+        md:text-sm
+
+    `}
+`
+
+const LineSeperator = styled.div`
+    width: 2px;
+    height: 45%
+    ${tw`
+        bg-gray-300
+        mr-2
+        ml-2
+        md:mr-5
+        md:ml-5
+    `}
+`
+
+
+
+
+
+export function BookCard() {
+    return <CardContainer>
+        <ItemContainer>
+            <Icon>
+                <FontAwesomeIcon icon={faCalendarAlt} />
+            </Icon>
+            <Name>Pick Up Date</Name>
+        </ItemContainer>
+        <ItemContainer>
+            <Icon>
+                <FontAwesomeIcon icon={faCalendarAlt} />
+            </Icon>
+            <Name>Return Name</Name>
+        </ItemContainer>
+        <Marginer direction="horizontal" margin="2em"/>
+        <Button text="Book Your Ride" />
+    </CardContainer>
+}
